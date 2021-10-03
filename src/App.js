2a32c81e23c26b33f8550042391b71e2569f09c9
mjmaurya <gcssm1999@gmail.com>
2021-10-03 20:09:24 +0530

    ECom Initiated

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MainComponent from "./conmponents/MainComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MainComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/conmponents/MainComponent.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+import Header from './Header';
+
+class MainComponent extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+      </div>
+    );
+  }
+}
+export default MainComponent;
